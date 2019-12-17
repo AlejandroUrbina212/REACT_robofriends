8e126ab94b8ddbc1833c3ab1a0c77707b1fe3180
luis2 <luis212urbina@gmail.com>
2019-12-17 17:52:54 -0600

    Updates

---
+++ precache-manifest.344db1bed8ecc75cb67f772ec38bbcee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c073494bc911a2bb6bf28d69168395",
+    "url": "/REACT_robofriends/index.html"
+  },
+  {
+    "revision": "34a66ad22e3580a6ae5c",
+    "url": "/REACT_robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dbfe665f72654cfe1c30",
+    "url": "/REACT_robofriends/static/css/main.5eb918a9.chunk.css"
+  },
+  {
+    "revision": "34a66ad22e3580a6ae5c",
+    "url": "/REACT_robofriends/static/js/2.5739b297.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/REACT_robofriends/static/js/2.5739b297.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbfe665f72654cfe1c30",
+    "url": "/REACT_robofriends/static/js/main.9f4d6131.chunk.js"
+  },
+  {
+    "revision": "c259715df48c74375bdc",
+    "url": "/REACT_robofriends/static/js/runtime-main.1dc3e9a1.js"
+  }
+]);
