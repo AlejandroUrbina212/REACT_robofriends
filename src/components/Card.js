cc713044a981a2122156d7787cce269132f2dc37
luis2 <luis212urbina@gmail.com>
2019-12-31 16:47:15 -0600

    app is now a PWA, lighthouse test passed with 10

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ function Card ({name, email, id}) {
     // Card function can only return one element
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
-            <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+            <img src={`https://robohash.org/${id}?size=200x200`} alt='robots'/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
